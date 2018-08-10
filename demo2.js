15b04101d0a7cae3ac2fe52ede10e7c3dee74702
nix <qiuwei@huobi.com>
2018-08-10 17:04:49 +0800

    feat

--- demo2.js
+++ demo2.js
@@ -28,24 +28,49 @@ const main = async () => {
         height: 768,
     })
 
-    console.info('跳转到wiki')
-    await page.goto('http://wiki.dataengine.com/', {
+    console.info('登录页')
+    await page.goto('http://localhost:8084', {
         //waitUntil: 'networkidle',
     })
 
-    const selectorOfUserNameInput = '#os_username'
-    const selectorOfPasswordInput = '#os_password'
-    const selectorOfloginButton = '#loginButton'
-    const options = { delay: 200 }
+    const selectorOfUserNameInput = 'input[name=name]'
+    const selectorOfPasswordInput = 'input[name=password]'
+    const selectorOfPasswordGooglePwdInput = 'input[name=googlePwd]'
+    const selectorOfloginButton = 'button[type=submit]'
+    const options = { delay: 1 }
 
     console.info('输入用户名')
-    await page.type(selectorOfUserNameInput, 'qiuwei', options)
+    await page.type(selectorOfUserNameInput, 'admin', options)
 
     console.info('输入密码')
-    await page.type(selectorOfPasswordInput, 'xxx', options)
+    await page.type(selectorOfPasswordInput, '654321', options)
+
+    if (page.$(selectorOfPasswordGooglePwdInput).value === '') {
+      console.info('输入Google验证码')
+      await page.type(selectorOfPasswordGooglePwdInput, '123456', options)
+    }
 
     console.info('按下登录')
     await page.click(selectorOfloginButton, options)
+    await page.waitForNavigation({
+      waitUntil: 'load'
+    })
+
+    console.info('点击运营系统')
+    await page.waitForSelector('a[href="javascript:top_show(183);"]')
+    await page.click('a[href="javascript:top_show(183);"]')
+
+    console.info('点击研究院审核列表')
+    await page.waitForSelector('a[href="/coinList/approval/institute/list"]')
+    await page.click('a[href="/coinList/approval/institute/list"]')
+
+    console.info('点击审核')
+    await page.waitForSelector('a[href="/coinList/approval/researchInstituteReview?projectCode=201808034207"]')
+    await page.click('a[href="/coinList/approval/researchInstituteReview?projectCode=201808034207"]')
+
+    console.info('填写备注')
+    await page.waitForSelector('textarea[name=remark]')
+    await page.type('textarea[name=remark]', '123456', options)
 }
 
 module.exports = main
